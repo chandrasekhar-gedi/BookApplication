2fe9a5f75711aafca1902ffa8066c50999fa5b3f
chandrasekhar-gedi <gedichandrasekhar24@gmail.com>
2025-11-21 11:16:20 +0530

    config the h2 data base

---
+++ src/main/java/com/example/BookApplication/Controller/BookController.java
@@ -0,0 +1,28 @@
+package com.example.BookApplication.Controller;
+
+import com.example.BookApplication.Entity.Book;
+import com.example.BookApplication.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book/v1")
+public class BookController{
+   private final BookService bookService;
+   @Autowired
+   public BookController(BookService bookService) {
+       this.bookService = bookService;
+   }
+    @PostMapping("/addBook")
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+       Book savedBook = bookService.addBook(book);
+    return  ResponseEntity.ok(savedBook);
+    }
+
+}
